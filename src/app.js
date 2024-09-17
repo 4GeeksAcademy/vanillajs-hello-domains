cc1d553c5ca18670cef29bc0c87df61ad7c3b712
rcbarreto <69176511+rcbarreto@users.noreply.github.com>
2024-09-17 23:41:07 +0000

    dominios

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let ext = [".com", ".es", ".uy", ".ve", ".ar", ".co"];
-pronoun.forEach((comienzo) => {
-  adj.forEach((medio) => {
-    noun.forEach((final) => {
-      ext.forEach((exten) => {
-        let dominios = `${comienzo}${medio}${final}${exten}`;
-        console.log(dominios);
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".es", ".uy", ".ve", ".ar", ".co"];
+  let dominios = [];
+
+  pronoun.forEach(comienzo => {
+    adj.forEach(medio => {
+      noun.forEach(final => {
+        ext.forEach(exten => {
+          dominios.push(`${comienzo}${medio}${final}${exten}`);
+        });
       });
     });
   });
-});
+
+  let elementos = dominios.map(dominio => {
+    return `<li class="alert alert-warning">` + dominio + `</li>`;
+  });
+
+  document.getElementById("listadominios").innerHTML = elementos.join(" ");
+};
